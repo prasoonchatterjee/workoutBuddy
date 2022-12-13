239df689c09f1af1d039081e67d6f9a63d275ad3
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-13 10:58:45 +0530

    database initialized and connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const workoutRoutes = require('./routes/workouts');
 
 //initialize express app
@@ -9,7 +11,7 @@ const app = express();
 // middlewares
 
 //to access incoming data in req body
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -19,7 +21,15 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db and listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
