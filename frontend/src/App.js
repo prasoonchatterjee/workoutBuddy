e39d4a3cac463ac15537dda616086853b35cb0f0
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-14 08:44:09 +0530

    frontend app setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <p>frontend</p>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
