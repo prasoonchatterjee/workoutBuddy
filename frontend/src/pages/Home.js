9a89ed1d0935555e01468589eff4d59229e228ca
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-14 18:36:42 +0530

    add workout form integrated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -23,6 +24,7 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
