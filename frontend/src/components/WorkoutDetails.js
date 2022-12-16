c8fdf07107f1aad469dcd16e0aee99515e05add9
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-16 13:54:31 +0530

    delete workout added

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,16 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleDelete = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      const json = await response.json();
+      dispatch({ type: 'DELETE_WORKOUT', payload: json });
+    }
+  };
   return (
     <div className='workout-details'>
       <h4>{workout.title}</h4>
@@ -11,6 +23,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span onClick={handleDelete}>delete</span>
     </div>
   );
 };
