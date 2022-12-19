cf74cfdac753b79d70e2eb54949590852458c6c7
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-19 17:58:06 +0530

    signup setup with hash password and validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,18 @@
+const User = require('../models/userModel');
+
 // login user
 const loginUser = (req, res) => {
   res.status(200).json({ msg: 'User logined' });
 };
 // signup user
-const signupUser = (req, res) => {
-  res.status(200).json({ msg: 'User sign up' });
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
