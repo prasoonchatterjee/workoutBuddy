083047f775c17cdd30c1f7200413780e3baaeca3
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-21 18:34:18 +0530

    logout implemented

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 const Navbar = () => {
+  const { logout } = useLogout()
+
   return (
     <header>
       <div className='container'>
@@ -8,6 +11,9 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={logout}>Log out</button>
+          </div>
           <div>
             <Link to='/login'>Login</Link>
             <Link to='/signup'>Signup</Link>
